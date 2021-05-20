53c44afd6657fa63e0c9ef5df65153a104af7c13
acosta187 <acostacortez@gmail.com>
2021-05-20 15:55:16 -0500

    Sibling-cross Recognozible and notRecognozible for snaq

--- NetworkCharacterization/SibCrossIdentSnaq.r
+++ NetworkCharacterization/SibCrossIdentSnaq.r
@@ -36,7 +36,8 @@ bdNS <- bdNS[!sapply(X = bdNS, FUN = is.null)]
 bdNS <- bdNS[sapply(X = bdNS, FUN = is.phylo)]#birth dead tree with NetSim with value 0 or 1
  
 
-#Squeme of a sibling Reticulation
+
+#Squeme of evaluation if a sibling Reticulation is Recognosible
  
 #			
 #				 _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _[Tip or Node 4]     
@@ -45,6 +46,7 @@ bdNS <- bdNS[sapply(X = bdNS, FUN = is.phylo)]#birth dead tree with NetSim with 
 #				|
 #  Root_ _ _ _[Node#]    _ _ _ _ _ _ _ _ _ _ _ _ _ _ _[Tip or Node 3]
 #				|		|
+#               |       |
 #				|		|
 #				|_ _ _ _RootRet			   
 #						|        _ _ _ RetNode1_ _ _ _[Tip or Node 2]
@@ -59,28 +61,31 @@ bdNS <- bdNS[sapply(X = bdNS, FUN = is.phylo)]#birth dead tree with NetSim with 
 
 #Then I evaluate if a RetNode1 and RetNode2 produce a Tip or Node
 
-#Finaly if number of tips >=2 the reticulation is not Recognosible otherwise is Recognosible.
+#Finaly if number of tips are >=2, the siblign reticulation is not Recognosible otherwise is Recognosible.
 
 
 
-#We want to know if the reticulation node produce or no a tip
-#This function goes in the direction of the tips omiting reticulations nodes until it finds one no reticulation node
+#We want to know if the reticulation node produce a Tip or Node
+##This function goes in the direction of the tips omiting reticulations nodes until it finds one no reticulation node
 DesJupRet<-function(ED,reff,father,allret){
 ti12<-reff
 Log=TRUE
 while(Log){
-ti12<-ED[ED[,1]==ti12&ED[,2]!=father,2][1]###Loop
+ti12<-ED[ED[,1]==ti12&ED[,2]!=father,2][1]
+#we use father because when we want to evaluate RootRet we want to know if [Tip or Node 3] is a node or tip, no go for the reticulation way.
 Log<-ti12%in%allret
 }
 return(ti12)
 }
 
-#This function 
+
+#This function goes in the direction of the tips jumping node by node and determinate if the refference (reff) produce a Tip or not
 Tip<-function(ED,reff,father,allret){
 Log=TRUE
 i=0
 while(Log){
 reff<-DesJupRet(ED=ED,reff=reff,father=father,allret=allret)
+#
 Log<-!(i==2|is.na(reff))
 i=i+1
 }
@@ -89,6 +94,7 @@ return(res)
 }
 
 
+##This function goes in the direction of the root omiting reticulations nodes until it finds one no reticulation node
 AssJupRet<-function(ED,reff,allret){
 ti12<-reff
 Log=TRUE
@@ -99,6 +105,7 @@ Log<-ti12%in%allret
 return(ti12)
 }
 
+#This function goes in the direction of the root jumping node by node and determinate if the refference (reff) produce a root or node
 AssRoot<-function(ED,reff,allret){
 Log=TRUE
 i=0
@@ -107,24 +114,27 @@ reff<-AssJupRet(ED=ED,reff=reff,allret=allret)
 Log<-!(i==2|is.na(reff))
 i=i+1
 }
-if(i==2){res<-"Root"}else if(i==3){res<-"NoRoot"}
+if(i==2){res<-"Root"}else if(i==3){res<-"NoRoot"}#else if (i<=1){res<-"No-Identificable"}
 return(res)
 }
 
-
-
+#This function use the functions described before and determinate if the sibling reticulation can be Recognozible by snaq
 Identiff<-function(Tree2,Hib1,Hib2,ED,father,RootTest,allret){
 Tiprev<-Tip(ED,reff=RootTest,father=father,allret=allret)
 Roo<-AssRoot(ED,reff=RootTest,allret=allret)
+#if(any(Roo%in%c("No-Identificable",NA,NULL))){resAll<-"No-Identificable"}else{
+
 NodTip1<-Tiprev=="NoTip"|Roo=="NoRoot"
 NodTip2<-Tip(ED,reff=Hib1,father=father,allret=allret)=="NoTip"
 NodTip3<-Tip(ED,reff=Hib2,father=father,allret=allret)=="NoTip"
+#res1<-sum(c(NodTip1,NodTip2,NodTip3),na.rm = TRUE)#using (+) whit NA or NULL error will be apear, but if we use sum(,na.rm = TRUE), we omith that.7
 res1<-NodTip1+NodTip2+NodTip3
-if(res1>=2){resAll<-"Identificable"}else {resAll<-"No-Identificable"}
-resAll
+if(res1>=2){resAll<-"Identificable"}else {resAll<-"No-Identificable"} #}
+return(resAll)
 }
 
 
+# This function analize the tree, determinate if there are reticulations and in the case that it exists evalaute if the reticulation is among siblings, and in the case that it exists evaluate if it is or not recognozible by snaq.
 
 SibCross<-function(Tree2){
 RetEv<-Tree2$"reticulation"
@@ -160,20 +170,24 @@ return(res)
 }
 
 
+#Tree2<-bdNS[[13]]
+#SibCross(Tree2)
+
+
 #No Identificable for snaq
 out<-c()
 for(i in 1:length(bdNS)){
 res<-SibCross(bdNS[[i]])$infor
-res2<-any(res=="No-Identificable")
+res2<-any(res=="Identificable")
 out<-c(out,res2)
 }
-which(out)
+
 
 posNotIdent1<-which(out);posNotIdent1
-TreeAnalize1<-bdNS[[posNotIdent1[2]]]
+TreeAnalize1<-bdNS[[posNotIdent1[4]]]
 
 sibcro1<-SibCross(TreeAnalize1)
-nod1<-sibcro1$ret[sibcro1$infor=="No-Identificable",]
+nod1<-sibcro1$ret[sibcro1$infor=="Identificable",]
 plot(TreeAnalize1)
 nodelabels(node=nod1)
 
@@ -182,19 +196,21 @@ nodelabels(node=nod1)
 
 
 
+
+
 #Identificable for snaq
 out2<-c()
 for(i in 1:length(bdNS)){
 res<-SibCross(bdNS[[i]])$infor
-res2<-any(res=="Identificable")
+res2<-any(res=="No-Identificable")
 out2<-c(out2,res2)
 }
 posNotIdent<-which(out2);posNotIdent
 
-TreeAnalize<-bdNS[[posNotIdent[4]]]
+TreeAnalize<-bdNS[[posNotIdent[2]]]
 
 sibcro<-SibCross(TreeAnalize)
-nod<-sibcro$ret[sibcro$infor=="Identificable",]
+nod<-sibcro$ret[sibcro$infor=="No-Identificable",]
 plot(TreeAnalize)
 nodelabels(node=nod)
 
