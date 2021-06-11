fb1ebb9fc86d3798b10afdde3cf83b8925833440
gaballench <gaballench@gmail.com>
2021-06-11 18:33:38 -0300

    first run for h=0 with Claudia's 15-leaf network, one hexacycle. It took 4 hours with a 4-thread i7 processor

--- snaq_samplesize_loci/h1_n6_ho_net0.jl
+++ snaq_samplesize_loci/h1_n6_ho_net0.jl
@@ -1,3 +1,4 @@
+### h1_n6_ho_net0.jl
 using Distributed
 addprocs(4)
 @everywhere using PhyloNetworks
@@ -9,7 +10,7 @@ using CSV
 trees = readMultiTopology("hybridLambda_output/net_h1_n6_ho_coal_unit")
 # terminals must be renamed in order to match gene-tree names from hybridLambda
 # also, an arbitrary position for the hybrid-induced terminal was chosen to be sister to G
-start_tree = readTopology("((((A_1,B_1),(C_1,D_1)),(E_1,(F_1,(G_1,Hhyb0.5_1)))),H_1);")
+start_tree = readTopology("(15_1,(1_1,((14_1,(((12_1,13_1),(11_1,H3hyb0.5_1)),(7_1,(10_1,(8_1,9_1))))),(((2_1,3_1),(6_1,(5_1,4_1)))))));")
 
 # calculate CFs
 q,t = countquartetsintrees(trees)
@@ -18,3 +19,5 @@ cfs = readTableCF(df)
 
 # calculate a h=0 network
 net0 = snaq!(start_tree, cfs, hmax=0, filename="h1_n6_ho_net0", seed=1234)
+
+exit()
