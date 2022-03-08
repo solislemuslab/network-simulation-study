d0cf7d4eeb1e09e1e769ad2a156ac5ab257d376a
gaballench <gaballench@gmail.com>
2022-03-08 19:47:45 +0000

    Prepared the script for format conversion, one network file at a time

--- level_2/pipeline/extnewick2hybridlambda.jl
+++ level_2/pipeline/extnewick2hybridlambda.jl
@@ -1,29 +1,33 @@
-#GAB we need to modify this script so that it has two arguments: the file name of the extnewick which comes from SiPhyNetwork, and a filename for conversion to hybridlambda
-#GAB Note that this script should be then operating over one single file at a time: One in, one out. The R script generate_datasets will take care of iterations
-r_net_path = ARGS[1]
-ju_net_path = ARGS[2]
-hyla_format_path = ARGS[3]
+"""
+extnewick2hybridlambda.jl 
 
-cd(r_net_path)#cd("/home/acosta/GitProjects/network-simulation-study/BashScript_Network_NotLevelOne/RNetworks")
+Script for format conversion between
+extended newick (input) and hybrid-Lambda (output)
+
+This script reads the first argument as the path to 
+the input file, and the second as the path to the 
+output file.
+
+example
+
+$ julia extnewick2hybridlambda.jl input.extnewick input.hybridlambda
+"""
+
+input_net_path = ARGS[1]
+output_net_path = ARGS[2]
+
 using PhyloNetworks, PhyloPlots
 using RCall
 
-#net0 = readTopology("RNetwork")
-Nets=split(read(`ls`, String))
+# read the input netowrk in extnewick
+input_net = readTopology(input_net_path)
 
+# convert to hybridlambda format
+output_net = hybridlambdaformat(input_net)
 
-for i in 1:length(Nets)
-	cd(r_net_path)
-	RNet = Nets[i]
-	net0 = readTopology(RNet)
-	namefileJu = replace(RNet,"RNetwork"=>"RJuliaNet")*".txt"
-	namefileHy = replace(RNet,"RNetwork"=>"JulHybrLamb")
-	cd(ju_net_path)#ju
-	writeTopology(net0,namefileJu)
-	cd(hyla_format_path)#hyla
-	HyLaF = hybridlambdaformat(net0)
-	open(namefileHy, "w") do io
-		write(io, HyLaF)
-	end
-	cd(r_net_path)
-end 
+# write to file
+open(output_net_path, "w") do io
+    write(io, output_net)
+end
+
+exit()
