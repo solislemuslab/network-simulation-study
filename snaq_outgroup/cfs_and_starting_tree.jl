9501f220f71f751fa2da8324dcaf898771be6916
gaballench <gaballench@gmail.com>
2022-03-11 18:17:14 +0000

    Simulations for the study on the spurious appearance of reticulations around the root running.

---
+++ snaq_outgroup/cfs_and_starting_tree.jl
@@ -0,0 +1,54 @@
+"""
+cfs_and_starting_treer.jl
+
+Script for calculating the concordance factor table 
+with PhyloNetworks and the initial tree with TICR's
+get-pop-tree.pl script which uses QMC.
+
+Usage:
+
+ julia cfs_and_starting_tree.jl path-to-genetrees-file.hybridlambda
+
+Note: get-pop-tree.pl must be in the path. It should also be executable
+"""
+
+using PhyloNetworks
+using CSV
+using DataFrames
+
+gtfilename = ARGS[1]
+
+println("Processing $gtfilename...")
+
+# load input trees
+trees = readMultiTopology(gtfilename)
+
+# calculate CFs
+q,t = countquartetsintrees(trees)
+df = writeTableCF(q,t)
+cfs = readTableCF(df)
+
+# add dummy columns to df so that it is get-pop-tree.pl-compliant
+zeros = repeat([0], nrow(df))
+
+df_zeros = DataFrame(CF12_34_lo=zeros,
+                     CF12_34_hi=zeros,
+                     CF13_24_lo=zeros,
+                     CF13_24_hi=zeros,
+                     CF14_23_lo=zeros,
+                     CF14_23_hi=zeros)
+
+df_ticr = hcat(select(df, 1:5),
+               select(df_zeros, 1:2),
+               select(df, 6),
+               select(df_zeros, 3:4),
+               select(df, 7),
+               select(df_zeros, 5:6))
+
+CSV.write("CFs_$gtfilename.csv", df_ticr)
+
+# calculate the starting tree using part of the TICR pipeline
+# get-pop-tree.pl must be in the path
+run(Cmd(["get-pop-tree.pl", "CFs_$gtfilename.csv"]))
+
+exit()
--- snaq_outgroup/pipeline.sh
+++ snaq_outgroup/pipeline.sh
@@ -8,7 +8,17 @@ for i in `ls cropped_tree*.newick`; do
     hybrid-Lambdav0.6.2 -spcu $i -num 3000 -seed 1453 -o ${i}_gene_trees
 done
 
-# run snaq on the gene trees from hybrid-Lambda with the init-tree from QMC
+# calculate CFs with PhyloNetworks and init_tree with TICR
 for i in `ls *.newick_gene_trees_coal_unit`; do
-    julia snaq_cropped_tree.jl $i
+    julia cfs_and_starting_tree.jl $i
 done
+
+# prepare the pbs scripts using the template file template.pbs
+for i in `ls CFs_*csv`; do
+    # create the pbs script from the template file and just append the extension .pbs
+    touch $i.pbs
+    cat template.pbs > $i.pbs
+    # replace the placeholder JOBID with the CF filename
+    sed -i "s/JOBID/$i/g" $i.pbs
+done
+
