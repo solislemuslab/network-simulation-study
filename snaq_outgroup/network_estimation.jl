53a5ed07246e7c7de57d73b4712d50369fc3877c
gaballench <gaballench@gmail.com>
2022-03-13 17:57:02 +0000

    Updating minor fixes which were necessary but only discovered while runs failed in the cluster. Now all experiments should be running fine.

--- snaq_outgroup/network_estimation.jl
+++ snaq_outgroup/network_estimation.jl
@@ -43,7 +43,7 @@ cfs = readTableCF("$cf_table")
 start_tree = readTopology("$init_tree")
 
 # calculate a h=1 network
-net = snaq!(start_tree, cfs, hmax=h, filename="snaq_output_h$h", seed=seed, runs = nruns)
+net = snaq!(start_tree, cfs, hmax=h, filename="snaq_output_h$(h)_$(cf_table)", seed=seed, runs = nruns)
 
 #exit julia
 exit()
